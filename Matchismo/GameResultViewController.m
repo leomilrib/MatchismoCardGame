db50e9b6328397abefd06080abe05c175d0cb10d
leomilrib <leomilrib@gmail.com>
2013-09-11 17:03:56 -0300

    Add codes from Stanford.

--- Matchismo/GameResultViewController.m
+++ Matchismo/GameResultViewController.m
@@ -6,15 +6,72 @@
 //
 
 #import "GameResultViewController.h"
+#import "GameResult.h"
 
 @interface GameResultViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextView *display;  // wire this up to a UITextView
+@property (nonatomic) SEL sortSelector; // added after lecture
 @end
 
 @implementation GameResultViewController
+-(void)updateUI{
+  NSString *displayText = @"";
+  NSDateFormatter *formatter = [[NSDateFormatter alloc] init]; // added after lecture
+  [formatter setDateStyle:NSDateFormatterShortStyle];          // added after lecture
+  [formatter setTimeStyle:NSDateFormatterShortStyle];          // added after lecture
+  // for (GameResult *result in [GameResult allGameResults]) { // version in lecture
+  for (GameResult *result in [[GameResult allGameResults] sortedArrayUsingSelector:self.sortSelector]) { // sorted
+    // displayText = [displayText stringByAppendingFormat:@"Score: %d (%@, %0g)\n", result.score, result.end, round(result.duration)]; // version in lecture
+    displayText = [displayText stringByAppendingFormat:@"Score: %d (%@, %0g)\n", result.score, [formatter stringFromDate:result.end], round(result.duration)];  // formatted date
+  }
+  self.display.text = displayText;
+}
+
+#pragma mark - View Controller Lifecycle
+
+-(void)viewWillAppear:(BOOL)animated{
+  [super viewWillAppear:animated];
+  [self updateUI];
+}
+
+// Sorting section added after lecture.
+// See also the Sorting section in GameResult.[mh].
+// Wire up the three IBActions to the three buttons in the View.
+
+#pragma mark - Sorting
+
+@synthesize sortSelector = _sortSelector;  // because we implement BOTH setter and getter
+
+// return default sort selector if none set (by score)
+
+-(SEL)sortSelector{
+  if (!_sortSelector) _sortSelector = @selector(compareScoreToGameResult:);
+  return _sortSelector;
+}
+
+// update the UI when changing the sort selector
+
+-(void)setSortSelector:(SEL)sortSelector{
+  _sortSelector = sortSelector;
+  [self updateUI];
+}
+
+-(IBAction)sortByDate{
+  self.sortSelector = @selector(compareEndDateToGameResult:);
+}
+
+-(IBAction)sortByScore{
+  self.sortSelector = @selector(compareScoreToGameResult:);
+}
+
+-(IBAction)sortByDuration{
+  self.sortSelector = @selector(compareDurationToGameResult:);
+}
+
+#pragma mark - (Unused) Initialization before viewDidLoad
 
 -(void)setup{
-    //initializatoin that can't wait for viewDidLoad.
+  // initialization that can't wait until viewDidLoad
 }
 
 -(void)awakeFromNib{
@@ -22,21 +79,37 @@
 }
 
 -(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-//    if (self) {
-    [self setup];
-//    }
-    return self;
-}
-
--(void)viewDidLoad{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
--(void)didReceiveMemoryWarning{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  [self setup];
+  return self;
 }
 
 @end
+
+//{
+//-(void)setup{
+//    //initializatoin that can't wait for viewDidLoad.
+//}
+//
+//-(void)awakeFromNib{
+//  [self setup];
+//}
+//
+//-(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+////    if (self) {
+//    [self setup];
+////    }
+//    return self;
+//}
+//
+//-(void)viewDidLoad{
+//    [super viewDidLoad];
+//	// Do any additional setup after loading the view.
+//}
+//
+//-(void)didReceiveMemoryWarning{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
+//}
--- Matchismo/Model/GameResult.h
+++ Matchismo/Model/GameResult.h
@@ -5,8 +5,20 @@
 //  Created by leo on 9/11/13.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface GameResult : UIViewController
+@interface GameResult : NSObject
+
+
++(NSArray *)allGameResults; // of GameResult
+
+@property (readonly, nonatomic) NSDate *start;
+@property (readonly, nonatomic) NSDate *end;
+@property (readonly, nonatomic) NSTimeInterval duration;
+@property (nonatomic) int score;
+
+-(NSComparisonResult)compareScoreToGameResult:(GameResult *)otherResult;
+-(NSComparisonResult)compareEndDateToGameResult:(GameResult *)otherResult;
+-(NSComparisonResult)compareDurationToGameResult:(GameResult *)otherResult;
 
 @end
